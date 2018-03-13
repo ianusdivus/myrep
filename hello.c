a0ac174a76927070fc40945dca3995011ddebb7a
ianusdivus <janmeiners@gmx.de>
2018-03-13 21:38:10 +0100

    added new files (first programs), edited source code

--- hello.c
+++ hello.c
@@ -1,14 +1,16 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 //------------------ function declaration area --------------
 
 // print function f(xy)= 2*x+y
 int printvariables (int x, int y) {
-  printf ("f(%d,%d) is %d", x, y, 2*x+y);
+  printf ("f(%d,%d) is %d\n", x, y, 2*x+y);
+  return 0;
     }
 
-//------------------ main function beginnung here-------------
+//------------------ main function beginning here-------------
 
 int main (void) {
   printvariables (3, 4);
-  return 0;
+  return EXIT_SUCCESS;
 }
