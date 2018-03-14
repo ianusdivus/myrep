828c3f24eacc2d764f994ff6d308f5dd65f98c97
ianusdivus <janmeiners@gmx.de>
2018-03-14 20:55:13 +0100

    fehlerhafte aenderung

--- prime_checker.c
+++ prime_checker.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 
 //function declaration
-int isprime (n) {
+int isprime (int argc, int argv []) {
 
 // if n <= 1 the answer is no
-  if (n<=1){
+  if (argv[1]<=1){
     return 0;
   }
   // count from 2 to n (exclusive), call it i
-  for (int i=2;i<n;i++){
+  for (int i=2;i<argv[1];i++){
     //if n mod i equals 0 , return 0
-    if (n%i == 0){
+    if (argv[1]≈%i == 0){
       return 0;}}
   return 1;}
 
 int main (void) {
-  if (isprime(17) == 1){
+  if (isprime(n) == 1){
     printf ("entered number is prime");
   }
   else {
